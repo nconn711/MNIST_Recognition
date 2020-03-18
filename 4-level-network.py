a93daf90db2ad99a2ebb6bbefde53ff00d70fc0e
nconner2 <nconner2@illinois.edu>
2020-03-17 20:56:02 -0500

    gradient update

--- 4-level-network.py
+++ 4-level-network.py
@@ -3,12 +3,12 @@ import time
 
 start_time = time.time()
 
-with np.load('../Desktop/mnist_recognition/mnist.npz') as data:
+with np.load('mnist.npz') as data:
     training_images = data['training_images']
     training_labels = data['training_labels']
 
-learning_rate = .1
-iter_1 = 100
+learning_rate = .01
+iter_1 = 50
 iter_2 = 50
 counter = 0
 
@@ -17,8 +17,8 @@ class NeuralNetwork:
 
     def __init__(self, rows, columns=0):
         self.mtrx = np.zeros((rows, 1))
-        self.weight = np.random.random((rows, columns)) / columns ** .5
-        self.bias = np.random.random((rows, 1)) * -1.0
+        self.weight = np.random.normal(size=(rows, columns)) / (columns ** .5)
+        self.bias = np.random.random((rows, 1))
         self.grad = np.zeros((rows, columns))
 
     def sigmoid(self):
@@ -34,22 +34,23 @@ lvl_output = NeuralNetwork(10, 200)
 
 
 def forward_prop():
-    lvl_one.mtrx = lvl_one.weight.dot(lvl_input.mtrx) + lvl_one.bias
-    lvl_two.mtrx = lvl_two.weight.dot(lvl_one.sigmoid()) + lvl_two.bias
-    lvl_output.mtrx = lvl_output.weight.dot(lvl_two.sigmoid()) + lvl_output.bias
+    lvl_one.mtrx = lvl_one.weight.dot(lvl_input.mtrx)
+    lvl_two.mtrx = lvl_two.weight.dot(lvl_one.sigmoid())
+    lvl_output.mtrx = lvl_output.weight.dot(lvl_two.sigmoid())
 
 
 def back_prop(actual):
     val = np.zeros((10, 1))
     val[actual] = 1
 
-    delta_3 = (lvl_output.sigmoid() - val) * lvl_output.sigmoid_derivative()
-    delta_2 = np.dot(lvl_output.weight.transpose(), delta_3) * lvl_two.sigmoid_derivative()
-    delta_1 = np.dot(lvl_two.weight.transpose(), delta_2) * lvl_one.sigmoid_derivative()
+    error_1 = lvl_output.sigmoid() - val
+    lvl_output.grad += error_1 @ lvl_one.sigmoid().T
 
-    lvl_output.grad = lvl_two.sigmoid().transpose() * delta_3
-    lvl_two.grad = lvl_one.sigmoid().transpose() * delta_2
-    lvl_one.grad = lvl_input.sigmoid().transpose() * delta_1
+    error_2 = lvl_two.sigmoid_derivative() * (lvl_output.weight.T @ error_1)
+    lvl_two.grad += error_2 @ lvl_one.sigmoid().T
+
+    error_3 = lvl_one.sigmoid_derivative() * (lvl_two.weight.T @ error_2)
+    lvl_one.grad += error_3 @ lvl_input.mtrx.T
 
 
 def make_image(c):
@@ -67,6 +68,9 @@ def update():
     lvl_output.weight -= learning_rate * lvl_output.grad
     lvl_two.weight -= learning_rate * lvl_two.grad
     lvl_one.weight -= learning_rate * lvl_one.grad
+    lvl_output.grad = np.zeros(np.shape(lvl_output.grad))
+    lvl_two.grad = np.zeros(np.shape(lvl_two.grad))
+    lvl_one.grad = np.zeros(np.shape(lvl_one.grad))
 
 
 for batch_num in range(iter_1):
